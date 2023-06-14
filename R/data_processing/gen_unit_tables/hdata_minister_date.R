cb8f778c502210dc1ed0b6eb6c5720064839cc52
Demscore <103125501+demscore@users.noreply.github.com>
2023-06-14 13:41:46 +0200

    Update hdata_minister_date.R
    delete notes

--- R/data_processing/gen_unit_tables/hdata_minister_date.R
+++ R/data_processing/gen_unit_tables/hdata_minister_date.R
@@ -9,10 +9,6 @@ hdata_fomin <- read_datasets("hdata_fomin", db)
 # Check for duplicates
 no_duplicates(hdata_fomin, c("u_hdata_minister_date_date_in", "u_hdata_minister_date_minister"))
 
-#For this I am using date_in, but date out also uniquely identifies, and so would a combination of date_in and date_out
-#so if we want, we could interpolate between those dates for all dates a minister is in office.
-#Adding country to the unit table, although it's not necessary.
-
 #bind
 u_hdata_minister_date <- 
   bind_rows(select(hdata_fomin, 
