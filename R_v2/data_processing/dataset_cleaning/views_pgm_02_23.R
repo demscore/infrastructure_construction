4d55baffd3b9fc8040befc95f75425761c7dec06
Demscore <103125501+demscore@users.noreply.github.com>
2023-11-23 13:50:12 +0100

    Add files via upload

---
+++ R_v2/data_processing/dataset_cleaning/views_pgm_02_23.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(demutils)
+
+db <- pg_connect()
+
+views_pgm_02_23 <- read_datasets("views_pgm_02_23", db, original = TRUE)
+
+# Clean column names
+names(views_pgm_02_23) <- clean_column_names(names(views_pgm_02_23))
+
+# For now only keep two prediction variables per dataset
+views_pgm_02_23 %<>% select(pg_id, month_id, sc_pgm_sb_main, sc_pgm_sb_dich_main)
+
+# duplicate check to create unit identifiers
+no_duplicates(views_pgm_02_23, c("pg_id", "month_id"))
+
+# create unit identifiers
+views_pgm_02_23$u_views_pg_month_pg_id <- views_pgm_02_23$pg_id
+
+views_pgm_02_23$u_views_pg_month_month_id <- views_pgm_02_23$month_id
+
+# Final duplicates check column names
+no_duplicate_names(views_pgm_02_23)
+
+# Save file
+write_dataset(views_pgm_02_23, 
+              file.path(Sys.getenv("ROOT_DIR"),
+                        "datasets/ucdp/cleaned_datasets/views_pgm_02_23_cleaned.rds"),
+              tag= "views_pgm_02_23",
+              overwrite = TRUE)
+
+# Create static files in dta and csv format
+write_file(views_pgm_02_23,
+           file.path(Sys.getenv("ROOT_DIR"),
+                     "datasets/ucdp/cleaned_datasets_dta/views_pgm_02_23_cleaned.dta"),
+           overwrite = TRUE)
+
+write_file(views_pgm_02_23,
+           file.path(Sys.getenv("ROOT_DIR"),
+                     "datasets/ucdp/cleaned_datasets_csv/views_pgm_02_23_cleaned.csv"),
+           overwrite = TRUE)
---
+++ R_v2/data_processing/dataset_cleaning/views_pgm_07_22.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(demutils)
+
+db <- pg_connect()
+
+views_pgm_07_22 <- read_datasets("views_pgm_07_22", db, original = TRUE)
+
+# Clean column names
+names(views_pgm_07_22) <- clean_column_names(names(views_pgm_07_22))
+
+# For now only keep two prediction variables per dataset
+views_pgm_07_22 %<>% select(pg_id, month_id, sc_pgm_sb_main, sc_pgm_sb_dich_main)
+
+# duplicate check to create unit identifiers
+no_duplicates(views_pgm_07_22, c("pg_id", "month_id"))
+
+# create unit identifiers
+views_pgm_07_22$u_views_pg_month_pg_id <- views_pgm_07_22$pg_id
+
+views_pgm_07_22$u_views_pg_month_month_id <- views_pgm_07_22$month_id
+
+# Final duplicates check column names
+no_duplicate_names(views_pgm_07_22)
+
+# Save file
+write_dataset(views_pgm_07_22, 
+              file.path(Sys.getenv("ROOT_DIR"),
+                        "datasets/ucdp/cleaned_datasets/views_pgm_07_22_cleaned.rds"),
+              tag= "views_pgm_07_22",
+              overwrite = TRUE)
+
+# Create static files in dta and csv format
+write_file(views_pgm_07_22,
+           file.path(Sys.getenv("ROOT_DIR"),
+                     "datasets/ucdp/cleaned_datasets_dta/views_pgm_07_22_cleaned.dta"),
+           overwrite = TRUE)
+
+write_file(views_pgm_07_22,
+           file.path(Sys.getenv("ROOT_DIR"),
+                     "datasets/ucdp/cleaned_datasets_csv/views_pgm_07_22_cleaned.csv"),
+           overwrite = TRUE)
